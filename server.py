a71a2a10e12a701b1f0175e38b52ef3761fab1d3
Ricardo David Espinosa Molero <129440738+Adven6@users.noreply.github.com>
2024-10-12 09:37:21 +0000

    Mundo guardado 2024-10-12

--- server.py
+++ server.py
@@ -19,7 +19,9 @@
 
 
 
+Comando 1: curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
 
+Comando 2: echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
 
 
 
